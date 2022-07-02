1e25f6be9fe87892ad7a95f55e24cbe42a709e98
aniruddhparwal <aniruddhparwal@gmail.com>
2022-07-02 11:51:14 +0530

    Getting Started

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+var cors = require("cors");
+const cookieParser = require("cookie-parser");
+app.use(express.json());
+
+// if (process.env.NODE_ENV === "production") {
+//   var corsOptions = {
+//     origin: "https://task-managemnt.netlify.app",
+//     optionsSuccessStatus: 200,
+//     credentials: true,
+//   };
+//   app.options("*", cors(corsOptions));
+//   app.use(cors(corsOptions));
+// }
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+module.exports = app;
